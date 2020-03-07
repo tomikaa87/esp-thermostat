1a17985ddf5840a820c3009f88b0c97c11f58426
tomikaa87 <tomikaa87@gmail.com>
2020-03-07 13:42:06 +0100

    Remove remains of old keypad driver

--- src/keypad.h
+++ src/keypad.h
@@ -13,35 +13,14 @@
 
     You should have received a copy of the GNU General Public License
     along with esp-thermostat.  If not, see <http://www.gnu.org/licenses/>.
-    
+
     Author: Tamas Karpati
     Created on 2016-12-30
 */
 
 #pragma once
 
-#include <stdint.h>
-
-#define KEY_R1_C1                       1
-#define KEY_R1_C2                       2
-#define KEY_R1_C3                       4
-#define KEY_R2_C1                       8
-#define KEY_R2_C2                       16
-#define KEY_R2_C3                       32
-#define KEY_LONG_PRESS                  0x8000
-#define KEY_CODE_MASK                   0x7FFF
-
-enum {
-	KEY_PLUS = KEY_R1_C1,
-	KEY_MINUS = KEY_R1_C2,
-	KEY_MENU = KEY_R2_C1,
-	KEY_BOOST = KEY_R1_C3,
-	KEY_LEFT = KEY_R2_C3,
-	KEY_RIGHT = KEY_R2_C2
-};
-
-void keypad_init();
-uint16_t keypad_task();
+#include <cstdint>
 
 class Keypad
 {
