0e9f77936800c2526e789c065b5d3b1a6cf9b1a7
tomikaa87 <tomikaa87@gmail.com>
2020-10-25 17:31:12 +0100

    Attempt to fix zero degrees issue with enabled Blynk-over-SSL

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ void setup()
 
     static ApplicationConfig appConfig;
 
-    appConfig.firmwareVersion = VersionNumber{ 1, 2, 2 };
+    appConfig.firmwareVersion = VersionNumber{ 1, 2, 3 };
 
     appConfig.blynk.appToken = Config::Blynk::AppToken;
     appConfig.blynk.serverHostName = Config::Blynk::ServerHostName;
