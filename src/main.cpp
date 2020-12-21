6b9bb237ff6571abbf87547f29ce9887ae3a36d1
tomikaa87 <tomikaa87@gmail.com>
2020-12-21 14:09:29 +0100

    Update esp-iot-base

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ void setup()
 
     static ApplicationConfig appConfig;
 
-    appConfig.firmwareVersion = VersionNumber{ 1, 2, 6 };
+    appConfig.firmwareVersion = VersionNumber{ 1, 2, 7 };
 
     appConfig.blynk.appToken = Config::Blynk::AppToken;
     appConfig.blynk.serverHostName = Config::Blynk::ServerHostName;
