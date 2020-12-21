407b793bb7027cbf955ca54f8411c37ec94be96e
tomikaa87 <tomikaa87@gmail.com>
2020-12-21 11:30:49 +0100

    Improve WiFi connection stability

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ void setup()
 
     static ApplicationConfig appConfig;
 
-    appConfig.firmwareVersion = VersionNumber{ 1, 2, 5 };
+    appConfig.firmwareVersion = VersionNumber{ 1, 2, 6 };
 
     appConfig.blynk.appToken = Config::Blynk::AppToken;
     appConfig.blynk.serverHostName = Config::Blynk::ServerHostName;
