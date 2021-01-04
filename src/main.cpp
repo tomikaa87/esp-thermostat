34d33952ab5d6155f3a20452af327fa47b34d6a5
tomikaa87 <tomikaa87@gmail.com>
2021-01-04 15:08:11 +0100

    Update esp-iot-base (include base platformio.ini)

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ void setup()
 
     static ApplicationConfig appConfig;
 
-    appConfig.firmwareVersion = VersionNumber{ 1, 2, 13 };
+    appConfig.firmwareVersion = VersionNumber{ 1, 2, 14 };
 
     appConfig.blynk.appToken = Config::Blynk::AppToken;
     appConfig.blynk.serverHostName = Config::Blynk::ServerHostName;
