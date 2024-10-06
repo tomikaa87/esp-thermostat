51fb24908f467d4194cebca2975079dd829be8c8
tomikaa87 <tomikaa87@gmail.com>
2024-10-06 21:11:41 +0200

    Bump version to 1.7.8

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ void setup()
 {
     initializeTempSensor();
 
-    appConfig.firmwareVersion = VersionNumber{ 1, 7, 7 };
+    appConfig.firmwareVersion = VersionNumber{ 1, 7, 8 };
 
 #ifndef TEST_BUILD
     appConfig.logging.syslog.enabled = true;
