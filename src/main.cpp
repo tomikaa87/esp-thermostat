cc01248ae078cb9608daf1bb778f3d9afdcf8b7b
tomikaa87 <tomikaa87@gmail.com>
2025-02-09 16:37:11 +0100

    Update esp-iot-base

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ void setup()
 {
     initializeTempSensor();
 
-    appConfig.firmwareVersion = VersionNumber{ 1, 7, 8 };
+    appConfig.firmwareVersion = VersionNumber{ 1, 7, 9 };
 
 #ifndef TEST_BUILD
     appConfig.logging.syslog.enabled = true;
